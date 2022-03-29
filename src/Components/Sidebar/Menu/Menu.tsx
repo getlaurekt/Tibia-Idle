8aa9422fd3b570f241a1e5b8ba57f2d4cb3ebb36
unknown <getlaurekt@gmail.com>
2022-03-29 11:01:55 +0200

    Added new gifs to menu

--- src/Components/Sidebar/Menu/Menu.tsx
+++ src/Components/Sidebar/Menu/Menu.tsx
@@ -1,28 +1,46 @@
 import React from "react";
 import tw from "tailwind-styled-components";
 import worm from "../../../assets/gifs/worm.gif";
+import demon from "../../../assets/gifs/demon.gif";
+import poetry from "../../../assets/gifs/poetry.png";
+import training from "../../../assets/gifs/training.gif";
+import npc from "../../../assets/gifs/npc.gif";
+import shops from "../../../assets/gifs/shops.png";
+import market from "../../../assets/gifs/tibia coin.gif";
 
 const Menu = () => {
   return (
     <Container>
       <MenuList>
         <MenuItem>Statistics</MenuItem>
-        <MenuItem>Character</MenuItem>
+        <MenuItem>
+          <Icon src={npc} />
+          Character
+        </MenuItem>
         <MenuItem>
           <Icon src={worm} />
           Battle
         </MenuItem>
-        <MenuItem>Bosses</MenuItem>
-        <MenuItem>Shops</MenuItem>
-        <MenuItem>Quests</MenuItem>
-        <MenuItem>Market</MenuItem>
-        <MenuItem>Statistics</MenuItem>
-        <MenuItem>Character</MenuItem>
-        <MenuItem>Battle</MenuItem>
-        <MenuItem>Bosses</MenuItem>
-        <MenuItem>Shops</MenuItem>
-        <MenuItem>Quests</MenuItem>
-        <MenuItem>Market</MenuItem>
+        <MenuItem>
+          <Icon src={demon} />
+          Bosses
+        </MenuItem>
+        <MenuItem>
+          <Icon src={training} />
+          Training
+        </MenuItem>
+        <MenuItem>
+          <Icon src={shops} />
+          Shops
+        </MenuItem>
+        <MenuItem>
+          <Icon src={poetry} />
+          Quests
+        </MenuItem>
+        <MenuItem>
+          <Icon src={market} />
+          Market
+        </MenuItem>
       </MenuList>
     </Container>
   );
@@ -35,13 +53,16 @@ const Container = tw.nav`
 `;
 
 const MenuList = tw.ul`
-    h-64
+    space-y-3
+`;
+
+/* const MenuList = tw.ul`
     space-y-3
     overflow-y-scroll
     overscroll-contain
     snap-mandatory
     snap-y
-`;
+`; */
 
 const MenuItem = tw.li`
     flex
@@ -58,11 +79,12 @@ const MenuItem = tw.li`
     ease-in-out
     hover:text-primary
     hover:bg-gray-500
-    scroll-my-2
-    snap-always
-    snap-start
     cursor-pointer
 `;
+
+/* scroll - my - 2;
+snap - always;
+snap - start; */
 
 const Icon = tw.img`
     w-[16px]
